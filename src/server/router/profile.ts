96ec5bbb3286b8a344d036f1ae32a9e523bea73e
brian-nunez <bnunez9999@gmail.com>
2022-07-19 19:56:55 -0700

    fix(router): profile remove middleware

--- src/server/router/profile.ts
+++ src/server/router/profile.ts
@@ -3,14 +3,6 @@ import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 
 export const profileRouter = createRouter()
-  .middleware(async ({ ctx, next }) => {
-    // Any queries or mutations after this middleware will
-    // raise an error unless there is a current session
-    if (!ctx.session) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next();
-  })
   .query("getByTag", {
     input: z
       .object({
